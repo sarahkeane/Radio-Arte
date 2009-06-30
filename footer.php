c60091a6b6d5acfd66c308fadc8bf5a79ef2a9f9
keith <keith@firebellydesign.com>
2009-06-30 12:31:09 -0500

    template files

---
+++ footer.php
@@ -0,0 +1,18 @@
+<div id="footer">
+ 	<p>
+	&copy;2009 All Rights Reserved, 90.5 FM WRTE Radio Arte / 1401 W. 18th st. Chicago, IL 60608 / 312.455.9455 / Site Map
+	<br>
+	<br>
+	<strong>Main Sponsors:</strong> <a href="http://www.nationalmuseumofmexicanart.org/">National Museum of Mexican Art</a>, <a href="http://www.cpb.org/">Corporation for Pubic Broadcasting</a>, <a href="http://www.rrmtf.org/">McCormick Foundation, </a>
+	<br>
+	<a href="http://www.afterschoolmatters.org/">After School Matters</a>, <a href="http://www.joycefdn.org/">The Joyce Foundation</a>, <a href="http://www.wgnradio.com/community/neediestkidsfund/">WGN Neediest Kids Fund</a>, <a href="http://www.rwjf.org/">Robert Wood Johnson Foundation</a>
+	<br>
+	<strong>Partners:</strong> <a href="http://newroutes.org/node/12122"> Latino Public Radio Consortium</a>, <a href="http://www.nfcb.org/index.jsp">National Federation of Community Broadcasters</a>, <a href="http://newroutes.org/">New Routes to Community Health</a>, <a href="http://burnhamplan100.uchicago.edu/">Burnham Plan 100</a>
+	</p>
+	
+</div>
+
+</div>
+	
+</body>
+</html>
---
+++ template.php
@@ -0,0 +1,80 @@
+
+<?php include("header.php"); ?>
+<div class="clearfix">
+<div class="firstmenu" class="clearfix">
+	<ul>
+		<li><a href="/overview">Overview</a></li>
+		<li><a href="/bios">Bios</a></li>
+		<li><a href="/history">History</a></li>
+		<li class="last"><a href="/faq">FAQ</a></li>
+	</ul>
+</div>
+</div>
+
+
+<div id="content" class="clearfix">
+
+	<div id="main">
+		<div class="padding_left">
+		<h1>Quienes Somos</h1>
+		<h2>MISI&#211;N</h2>
+		<p>Radio Arte, 90.5 WRTE-FM, es una estaci&#243;n de radio comunitaria, biling&#252;e, operada por j&#243;venes Latinos que se dedica a dar voz a nuestra 
+			sociedad y a mostrar su pluralidad y diversidad. Radio Arte promueve el periodismo reponsable y la justicia social, otorga educaci&#243;n 
+			medi&#225;tica y transmite una programaci&#243;n que ofrece m&#250;sica y contenidos ignorados en los medios comerciales. Radio Arte brinda a sus 
+			radioescuchas una amplia programaci&#243;n abarcando una gran variedad de temas de inter&#233;s com&#250;n: pol&#237;tica, inmigraci&#243;n, arte, deportes, 
+			literatura, gastronom&#237;a y asuntos LGBT. Radio Arte promueve los sonidos emergentes del vasto g&#233;nero musical latino al programar rock en 
+			espa&#241;ol, ska, electr&#243;nica, y punk entre otros ritmos alternativos.
+		</p>
+		
+		<h1>About Us</h1>
+		<h2>WHO WE ARE</h2>
+		<p>Radio Arte, 90.5 WRTE-FM, is a Latino-owned, bilingual, youth-driven public radio station that works to advance the voices of our multi-layered society. 
+			Radio Arte does this by promoting socially conscious journalism, providing media-literacy training; and creating programming that showcases music, 
+			issues, and events that are not featured prominently by mainstream media. Our guiding principals have been to:
+		</p>
+		<ol>
+			<li>Create and deliver high quality experiential learning opportunities that enable Latino youth to competencies and life skills to pursue careers 
+				of their choice, particularly journalism and media.</li>
+			<li>Build a strong national network of Spanish language youth media creators.</li>
+			<li>Support Latino youth as they become resources to the larger community through their involvement in media and civic engagement.</li>
+		</ol>
+		
+		
+		<h2>OUR HISTORY</h2>
+		<p>
+			The National Museum of Mexican Art (NMMA) acquired this Class D radio station in 1996 from the Boys & Girls Club of Chicago (BGCC), and has since 
+			supported the various initiatives that have catapulted Radio Arte into a national award winning institution.<br/><br/>  
+			Since 1997, Radio Arte has offered a comprehensive bilingual one-year media-training program to hundreds of youth (ages 15-21). Through Radio 
+			Arte's training, young adults receive instruction in journalism basics and are taught to analyze a variety of social justice issues; as well, 
+			they gain hands-on technical experience in radio production. Radio Arte's public affairs programming is driven by young people that consider the 
+			airwaves a tool of empowerment for themselves and their communities.
+		</p>
+		
+		<h2>OUR FORMAT</h2>
+		<h3>Latin Alternative</h3>
+		<p>
+			Since the mid-90's, Radio Arte has positioned itself as the only radio station in Chicago that has fully embraced the eclectic range of Latin music. 
+			Defying the traditional expectations imposed upon Latino culture, Radio Arte gave voice to the emerging sounds of Rock en Espa&#241;ol that included ska, 
+			electronica, and punk among others.<br/><br/>
+			Highlighting local and international artists, Radio Arte embraced acts such as Ely Guerra, Jaguares, Soda Stereo, Maria Fatal, Pastilla, Ley de Hielo, 
+			Los Crudos, La Querida de Cortes, and Los Enemigos de Paquita. Radio Arte continues to explore the innovation and independence of the Latin Alternative 
+			music, while recognizing the roots of its movement.<br/><br/>
+			Our Latin Alternative music programming features cutting edge Spanish language music from the U.S & Latin America, along with retro hits.  Includes 
+			informative music segments and a public affairs block featuring syndicated programs, Pacifica's *Democracy Now & Radio Bilingue's *Linea Abierta, 
+			from 1-3pm.
+		</p>
+		
+		<h3>Urban</h3>
+		<p>
+			The Urban format begins at 7pm with the daily Hotmix represented by the offical DJs of 90.5 FM DJ's in styles ranging from Deep 
+			House, Acid, Hip-Hop, Old School Classics, House, Freestyle, and other eclectic Genres. The Official 90.5 DJs are: DJ Greedy "The Rock", DJ 
+			Rocktonic, DJ Felix V, Dj Master Marco, DJ Victor Lua, DJ Panic, DJ Chi Boy, DJ Aryez. You can also catch them on the Friday night Hot Mix 
+			Show from 9pm-12Midnight and Saturdays on "Hotmix DJ's Saturday Night" 8pm-12Midnight.
+		</p>
+	</div>
+	</div>
+
+	<?php include("sidebar.php"); ?>
+</div>
+
+<?php include("footer.php"); ?>
